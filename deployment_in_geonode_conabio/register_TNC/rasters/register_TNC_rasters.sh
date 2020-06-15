1ef1bc9b03829110b8858c684a3fc1278e692932
erick <palmoreck@gmail.com>
2020-06-15 15:05:51 -0500

    changing 48 to 40 classes

--- deployment_in_geonode_conabio/register_TNC/rasters/register_TNC_rasters.sh
+++ deployment_in_geonode_conabio/register_TNC/rasters/register_TNC_rasters.sh
@@ -7,8 +7,8 @@ state_code=$(echo $2|sed -n 's/.*madmexplus_\(.*\).tif/\1/;p')
 state=$(echo $state_code|grep $state_code name_equivalency.txt|cut -d' ' -f2)
 year=$(echo $2|sed -n 's/.*_landsat8_\(.*\)_2020.*/\1/;p')
 region="$(echo $state), Mexico, North America, Latin America"
-name="MAD-Mex_TNC_landsat8_$(echo $state)_lc_$(echo $year)_48_classes"
-title="Land cover map for the state of $(echo $state) TNC $(echo $year) 48 classes"
+name="MAD-Mex_TNC_landsat8_$(echo $state)_lc_$(echo $year)_40_classes"
+title="Land cover map for the state of $(echo $state) TNC $(echo $year) 40 classes"
 kw="MAD-Mex, Landsat8, GeoTIFF, WCS"
 abstract="Land cover and land cover change mapping is sourced by Landsat 8 satellite images. All available images for the required calendar years were acquired through the ESPA processing system and were provided as atmospherically corrected surface reflectance and are accompanied by quality layers assigning, amongst others, pixels obscured by clouds and cloud shadows. For the 7 granules (path/rows) covering Chiapas, 155 Landsat-8 scenes of 2015 were acquired. The 17 granules covering the peninsula of Yucatan delivered 381 Landsat-8 scenes. For 2019, a total of 543 scenes were acquired. Land cover and land cover change is generated using a novel semi automated workflow based on time series processing and optimized training data generation. The workflow consists of the following procedures, including data acquisition, time series generation and processing, spectral clustering, training data generation and optimization, supervised object-based classifications, and geometry harmonization. Temporal metrics are used to extract land cover change. Labels for the changes are extracted for t0 from the reference map. Labels for t1 are classified using the same land cover workflow."
 
