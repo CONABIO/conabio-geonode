0c35782c876f6572cd2f52e461334575bbc7e36c
erick <palmoreck@gmail.com>
2020-12-02 10:25:09 -0600

    zip dir fun

---
+++ python3_package_for_geonode/geonode_conabio/utils_os.py
@@ -0,0 +1,9 @@
+import zipfile
+import os
+
+def zip_dir_for_layer_and_style(zip_path, layer_path, style_path):
+    zipf = zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED)
+    zipf.write(style_path, os.path.basename(style_path))
+    zipf.write(layer_path, os.path.basename(layer_path))
+    zipf.close()
+    os.remove(layer_path)  
