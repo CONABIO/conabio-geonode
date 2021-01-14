3970337ff23ae85f16a3ec6ba76ba5b1b7ee1e21
erick <palmoreck@gmail.com>
2021-01-14 11:22:41 -0600

    checking if string is empty

--- python3_package_for_geonode/geonode_conabio/utils_text.py
+++ python3_package_for_geonode/geonode_conabio/utils_text.py
@@ -1,4 +1,5 @@
 import unicodedata
 
 def normalize_name_classes(string):
-    return unicodedata.normalize('NFKD', string).encode('ASCII','ignore').decode('utf-8')
+    if string:
+        return unicodedata.normalize('NFKD', string).encode('ASCII','ignore').decode('utf-8')
