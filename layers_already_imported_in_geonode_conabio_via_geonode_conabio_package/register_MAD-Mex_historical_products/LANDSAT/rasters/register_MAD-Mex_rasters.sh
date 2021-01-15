77b50588e06e2fbcc4a501152c03d841ce9206ce
erick <palmoreck@gmail.com>
2021-01-15 09:21:44 -0600

    removing keyword regarding #17

--- layers_already_imported_in_geonode_conabio_via_geonode_conabio_package/register_MAD-Mex_historical_products/LANDSAT/rasters/register_MAD-Mex_rasters.sh
+++ layers_already_imported_in_geonode_conabio_via_geonode_conabio_package/register_MAD-Mex_historical_products/LANDSAT/rasters/register_MAD-Mex_rasters.sh
@@ -9,7 +9,7 @@ year=$(echo $1|cut -d'/' -f7)
 region="Mexico, North America, Latin America"
 name="National_MAD-Mex_$(echo $3|tr "[:upper:]" "[:lower:]")_lc_$(echo $year)_$(echo $4)_classes"
 title="National land cover map $(echo $year) $(echo $4) classes"
-kw="MAD-Mex, $(echo $3), GeoTIFF, WCS"
+kw="MAD-Mex, $(echo $3), GeoTIFF"
 abstract="Land cover sourced by $(echo $3) satellite images. All available images for the required calendar years were acquired through the ESPA processing system and were provided as atmospherically corrected surface reflectance and are accompanied by quality layers assigning, amongst others, pixels obscured by clouds and cloud shadows. Land cover is generated using a novel semi automated workflow based on time series processing and optimized training data generation. The workflow consists of the following procedures, including data acquisition, time series generation and processing, spectral clustering, training data generation and optimization, supervised object-based classifications, and geometry harmonization. "
 
 echo $1
